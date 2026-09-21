3efcea67d094bbdf78847e238d188b736cd21f2c
chiranjeevi pattigidi <cpattigi@amd.com>
2026-09-21 14:13:22 +0530

    Update THEROCK_REPO to new repository name

--- build_tools/github_actions/bump_automation.py
+++ build_tools/github_actions/bump_automation.py
@@ -14,7 +14,7 @@ from urllib.parse import quote
 
 import requests
 
-THEROCK_REPO = "ROCm/TheRock"
+THEROCK_REPO = "amd-chiranjeevi/TheRock"
 THEROCK_MAIN_BRANCH = "main"
 
 BOT_NAME = "therockbot"
